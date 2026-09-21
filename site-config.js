011e4c6e734305a43053fc62abae0a91067ff709
Pusher <twoheart1222@gmail.com>
2026-09-21 17:09:26 +0800

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -5,6 +5,6 @@
  *     前端就會用同網域相對路徑呼叫，不再出現任何 workers.dev 網址。
  * 已快取的照片一律以相對路徑 /api/photo/<hash> 存放，顯示時才套用這個網址。
  */
-window.ELECTION_API_BASE = 'https://election-api.uprisevideoproduction.workers.dev';
+window.ELECTION_API_BASE = 'https://api.formosaobservatory.com';
 window.SITE_CONTACT_EMAIL = 'contact@formosaobservatory.com';
 window.SITE_DATA_EMAIL = 'contact@formosaobservatory.com';
